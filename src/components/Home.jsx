96d979afde6829da18caae5036ce824c111f8229
aymerod3009 <u201619084@upc.edu.pe>
2021-08-05 16:57:23 -0500

    implementation forNavbar

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div>
+            <Header/>
+            <Navbar/>
+            <Body/>
+            </div>
+        );
+    }
+}
+
+export default Home;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,5 @@
+import React, {Component} from "react";
+
+class Navbar extends Component {
+
+}
